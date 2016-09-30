b9be7e470ba713fee158fad5a3553535cb30f6cc
Sebastian Heath <sebastian.heath@gmail.com>
2016-09-30 13:06:55 -0400

    dominate

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
+
+import dominate
+from dominate.tags import *
+
 from flask import Flask
 from flask import render_template
 from flask import request
+
+import rdflib
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return "Hello!"
+    doc = dominate.document(title="Pompeii LOD: ")
+    with doc:
+        h1("Pompeii LOD")
+    return doc.render
 
