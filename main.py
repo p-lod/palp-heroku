3904beab48e76758d4008cae530aefd9e811bcd1
Sebastian Heath <sebastian.heath@gmail.com>
2016-09-30 13:37:10 -0400

    sparql

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from flask import request
 
 import rdflib
 
+ns = {"dcterms": "http://purl.org/dc/terms/",
+      "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+      "rdfs": "http://www.w3.org/2000/01/rdf-schema#" ,
+      "p-lod" : "http://digitalhumanities.umass.edu/p-lod/",
+      "p-lod-v" : "http://digitalhumanities.umass.edu/p-lod/vocabulary/",
+      "p-lod-e" : "http://digitalhumanities.umass.edu/p-lod/entities/"}
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'], defaults={'path': ''})
@@ -17,10 +24,21 @@ def hello(path):
     g = rdflib.Graph()
 
     result = g.parse("p-lod.nt", format="nt")
+    subjects = g.query(
+        """SELECT DISTINCT ?s ?label
+           WHERE {
+              ?s ?p ?o .
+              ?s rdfs:label ?label .
+              ?s rdf:type ?type .
+           } ORDER BY ?s""", initNs = ns)
 
     doc = dominate.document(title="Pompeii LOD: ")
     with doc:
         h1("Pompeii LOD")
         p(path)
+        p()
+        for row in subjects:
+            p(str(row.label)
+            
     return doc.render()
 
