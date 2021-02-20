f97d2b6745700dbcd649b3d5a081b161479d7a4e
Sebastian Heath <sebastian.heath@gmail.com>
2021-02-19 20:15:36 -0500

    getters

--- plodlib_examples.py
+++ plodlib_examples.py
@@ -1,5 +1,7 @@
 import plodlib
 
+
+# run through instantiating ids and printing standard info
 r_list = ['pompeii','r1','r1-i1','r1-i1-p1','bird','bogus_id_bogus']
 
 for r in r_list:
@@ -8,6 +10,16 @@ for r in r_list:
   Identifier: {c.identifier} (as passed: {c._identifier_parameter})
   Type: {c.type}
   Label: {c.label}
+  P-in-P URL: {c.p_in_p_url}
+  Wikidata URL: {c.wikidata_url}
 
 		''')
 
+# some direct calls
+
+print('Dog depicted where')
+print(plodlib.PLODResource('dog').get_depicted_where())
+
+print('r1-i9 depicts concepts')
+
+print(plodlib.PLODResource('r1-i9').get_depicted_concepts())
