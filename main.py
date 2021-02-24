38d4672f028f6c3018832ffab2f167bb8acde636
Alex Brenon <abrenon@umass.edu>
2021-02-23 21:11:42 -0500

    Adding ARC page templating

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ import pandas as pd
 
 import dominate
 from dominate.tags import *
+from dominate.util import raw
 
 from bs4 import BeautifulSoup
 
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect, url_for, after_this_request
+from flask import Flask, render_template, session, json, request, flash, redirect, url_for, after_this_request
+from flask_mysqldb import MySQL
 
 from string import Template
 
@@ -26,8 +25,19 @@ import markdown
 import rdflib as rdf
 from rdflib.plugins.stores import sparqlstore
 
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
 # install with python3 -m pip install git+https://github.com/p-lod/plodlib
 import plodlib
+
+import boxsdk
+import json
+from datetime import datetime
+import glob
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from markupsafe import escape
 
 
 ns = {"dcterms" : "http://purl.org/dc/terms/",
@@ -38,13 +48,31 @@ ns = {"dcterms" : "http://purl.org/dc/terms/",
 
 app = Flask(__name__)
 
-
-
 # Connect to the remote triplestore with read-only connection
 store = rdf.plugin.get("SPARQLStore", rdf.store.Store)(endpoint="http://52.170.134.25:3030/plod_endpoint/query",
                                                        context_aware = False,
                                                        returnFormat = 'json')
 g = rdf.Graph(store)
+
+# MySQL configurations using environment variables
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_USER']
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+mysql = MySQL(app)
+
+#Box API configurations using environment variables
+box_auth = boxsdk.JWTAuth(
+    client_id=str(os.environ["BOX_ID"]),
+    client_secret=str(os.environ["BOX_SECRET"]),
+    enterprise_id=str(os.environ["BOX_ENTERPRISE"]),
+    jwt_key_id=str(os.environ["BOX_PUBLIC_KEY"]),
+    rsa_private_key_data=str(os.environ["BOX_PRIVATE_KEY"]).replace("||n||", "\n").replace("beginnnn", "BEGIN ENCRYPTED PRIVATE KEY").replace("endddd", 'END ENCRYPTED PRIVATE KEY'),
+    rsa_private_key_passphrase=str(os.environ["BOX_PASSPHRASE"])
+)
+
+box_access_token = box_auth.authenticate_instance()
+box_client = boxsdk.Client(box_auth)
 
 
 def palp_html_head(r, html_dom):
@@ -281,7 +309,68 @@ def space_render(r,html_dom):
 def feature_render(r,html_dom):
 
   with html_dom:
+    ar = r.identifier
+    totimgs = {}
+    pinpCur = mysql.connection.cursor()
+    pinpQuery = "SELECT  `archive_id`, `hero_image` FROM `PinP_preq` WHERE `ARC`='" + ar +"' OR `other_ARC` LIKE '%" + ar + "%';"
+    pinpCur.execute(pinpQuery)
+    pinpdata = pinpCur.fetchall()
+    pinpCur.close()
+    for d in pinpdata:
+      indpinp = {}
+      if str(d[1]) == "1":
+        indpinp['is_hero'] = True
+      else:
+        indpinp['is_hero'] = False
+      assocCur = mysql.connection.cursor()
+      assocQuery = "SELECT DISTINCT `id_box_file`, `img_alt` FROM `PinP` WHERE `archive_id` = '"+str(d[0])+"' ORDER BY `img_url`;"
+      assocCur.execute(assocQuery)
+      all0 = assocCur.fetchall()
+      indpinp['description'] = all0[0][1]
+      indpinp['box_id'] = all0[0][0]
+      assocCur.close()
+      totimgs[d[0]] = indpinp
+    ppmCur = mysql.connection.cursor()
+    ppmQuery = "SELECT  `id`, `hero_image` FROM `PPM_preq` WHERE `ARC`='" + ar +"' OR `other_ARC` LIKE '%" + ar + "%';"
+    ppmCur.execute(ppmQuery)
+    ppmdata = ppmCur.fetchall()
+    ppmCur.close()
+    for d in ppmdata:
+      indppm = {}
+      if str(d[1]) == "1":
+        indppm['is_hero'] = True
+      else:
+        indppm['is_hero'] = False
+      assocCur = mysql.connection.cursor()
+      assocQuery = "SELECT DISTINCT `image_id`, `translated_text` FROM `PPM` WHERE `id` = '"+str(d[0])+"';"
+      assocCur.execute(assocQuery)
+      all0 = assocCur.fetchall()
+      indppm['description'] = all0[0][1]
+      indppm['box_id'] = all0[0][0]
+      assocCur.close()
+      totimgs[d[0]] = indppm
 
+    with div(id="images"):
+      span("Images: ")
+      element = span()
+      with element:
+        for i in totimgs:
+          # try:
+          #   boxlink = box_client.file(totimgs[i]['box_id']).get_shared_link(access='collaborators')
+          # except boxsdk.BoxAPIException as exception:
+          #   boxlink=exception
+          # div(raw("""<iframe
+          #   src="{}?view=icon"
+          #   frameborder="0"
+          #   allowfullscreen
+          #   webkitallowfullscreen
+          #   msallowfullscreen
+          # ></iframe>""".format(boxlink)))
+          boxlink = "https://app.box.com/file/"+str(totimgs[i]['box_id'])
+          a(totimgs[i]['description'], href=boxlink)
+          if totimgs[i]['is_hero']:
+            span("-- is hero")
+          br()    
     if r.geojson:
       with div(id="geojson"):
         palp_geojson(r)[0:20]
